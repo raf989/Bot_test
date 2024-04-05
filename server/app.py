5b245d560b439182b55a6749338a4914527931ee
raf989 <mgraf276@gmail.com>
2024-04-06 01:42:50 +0300

    little refactoring

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ bots = dict()
 
 
 @app.post("/start", status_code=201)
-def read_root(data=Body()):
+def start(data=Body()):
     args = [sys.executable, 'bot/bot.py']
     if "start_number" in data:
         args.append(str(data["start_number"]))
@@ -19,12 +19,12 @@ def read_root(data=Body()):
 
 
 @app.get("/stop/{bot_id}", status_code=204)
-def read_root(bot_id: int):
+def stop(bot_id: int):
     if bot_id not in bots:
         return JSONResponse({"result": f"Bot {bot_id} is not found."}, status_code=404)
     bots.pop(bot_id).kill()
 
 
-@app.get("/bots")
-def read_root():
+@app.get("/active_bots")
+def active_bots():
     return JSONResponse({"active_bots": list(bots.keys())})
